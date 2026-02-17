1549b1cd597e0f0029757aaa4d762aa241b2a339
Vansh <narulavansh430@gmail.com>
2026-02-17 16:30:12 +0530

    Navbar menu icon functionality

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,9 @@
 import Logo from './Logo.png'
+import search_icon from './search_icon.png'
+import menu_icon from './menu_icon.png'
 
 export const assets={
-    Logo
+    Logo,
+    search_icon,
+    menu_icon
 }
