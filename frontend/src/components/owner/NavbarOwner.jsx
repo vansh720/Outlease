8bce250b5ef08f9b1adf0e639d49327a8eca58bd
Vansh <narulavansh430@gmail.com>
2026-03-14 01:07:52 +0530

    Added api in frontend

--- frontend/src/components/owner/NavbarOwner.jsx
+++ frontend/src/components/owner/NavbarOwner.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { assets, dummyUserData } from '../../assets/assets'
+import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import { useAppContext } from '../../context/AppContext';
 
 const NavbarOwner = () => {
-    const user= dummyUserData;
+    const {user}=useAppContext();
   return (
     <div className='flex items-center justify-between px-6 md:px-10 py-1 text-white relative border-b transition-all'>
       <Link to='/'>
          <img src={assets.Logo} alt="" className='h-16 w-auto'/>
       </Link>
-      <p className='text-black'>Welcome,{user.name || "Owner"}</p>
+      <p className='text-black'>Welcome,{user?.name || "Owner"}</p>
     </div>
   )
 }
