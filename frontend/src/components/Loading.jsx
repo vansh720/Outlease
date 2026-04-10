5ca5c5e05fcbb12aa94ff2f6e5dc3696d3bd23fc
Vansh <narulavansh430@gmail.com>
2026-04-11 01:17:09 +0530

    Added a Loader

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Loading = () => {
-
-  const {nextUrl}=useParams()
-  const navigate =useNavigate()
-  useEffect(()=>{
-    if(nextUrl){
-      setTimeout(()=>{
-        navigate('/'+nextUrl)
-      },2000)
+  const { nextUrl } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (nextUrl) {
+      setTimeout(() => {
+        navigate("/" + nextUrl);
+      }, 4000);
     }
-  },[])
-  
-  return (
-    <div className='flex justify-center items-center h-[80vh]'>
-        <div className='animate-spin rounded-full h-14 w-14 border-2 border-green-600'></div>
-    </div>
-  )
-}
+  }, []);
 
-export default Loading
+  return (
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className="animate-spin rounded-full h-14 w-14 border-4 border-gray-300 border-t-green-600"></div>
+    </div>
+  );
+};
+
+export default Loading;
